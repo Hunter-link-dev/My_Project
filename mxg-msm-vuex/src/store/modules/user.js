72b7637d2555254ad2f2afe6be548e547c065dfb
Douglas_Ryan <57554030+Hunter-link-dev@users.noreply.github.com>
2020-02-29 11:58:01 +0800

    Create user.js

---
+++ mxg-msm-vuex/src/store/modules/user.js
@@ -0,0 +1,59 @@
+import {getToken, setToken, getUser, setUser, removeToken} from '@/utils/auth'
+import {login, getUserInfo, logout} from "@/api/login"
+
+const user = {
+    state:{
+        token:getToken(),//这个初始的值会从localstoreage 这里进行获取
+        user:getUser(),
+    },
+    mutations:{
+        SET_TOKEN(state,token){
+            state.token = token
+            setToken(token)
+        },
+        SET_USER(state,user){
+            state.user = user
+            setUser(user)
+        }
+    },
+    actions:{
+        Login({commit},form){
+            return new Promise((resolve,reject) => {
+                login(form.username.trim(),form.password).then(response => {
+                    const resp = response.data;
+                    commit('SET_TOKEN',resp.data.token)
+                    resolve(resp)
+                }).catch(error => {
+                    reject(error)
+                })
+            })
+        },
+        GetUserInfo({commit, state}){
+            return new Promise((resolve,reject) => {
+                getUserInfo(state.token).then(response => {
+                    const respUser = response.data;
+                    commit('SET_USER',respUser.data)
+                    resolve(respUser)
+                }).catch(error => {
+                    reject(error)
+                })
+            })
+        },
+        //退出
+        Logout({commit, state}){
+            return new Promise((resolve,reject) => {
+                logout(state.token).then(response => {
+                    const resp = response.data;
+                    commit('SET_TOKEN','')
+                    commit('SET_USER',null)
+                    removeToken()
+                    resolve(resp)
+                }).catch(error => {
+                    reject(error)
+                })
+            })
+        },
+    }
+}
+
+export default user
