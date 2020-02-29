2238b3398c17d083af59f98e7fee83b809836a50
Douglas_Ryan <57554030+Hunter-link-dev@users.noreply.github.com>
2020-02-29 11:57:54 +0800

    Create supplier.js

---
+++ mxg-msm-vuex/src/api/supplier.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+export default {
+    getList(){
+        return request({
+        url: '/supplier/list',
+        method:'get',
+        })
+    },
+    search(page,size,searchMap){
+        return request({
+        url: `/supplier/list/search/${page}/${size}`,
+        method:'post',
+        data:searchMap
+        })
+    },
+    add(pojo){
+        return request({
+        url: '/supplier',
+        method:'post',
+        data:pojo,
+        })
+    },
+    getById(id){
+        return request({
+        url: `/supplier/${id}`,
+        method:'get'
+        })
+    },
+    update(pojo){
+        return request({
+        url: `/supplier/${pojo.id}`,
+        method:'put',
+        data:pojo
+        })
+    },
+    // 删除数据
+    deleteById(id){
+        return request({
+        url: `/supplier/${id}`,
+        method:'delete'
+        })
+    },
+}
