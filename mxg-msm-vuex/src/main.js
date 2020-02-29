1649d6a87c7c0ebc577961193db775449e16f359
Douglas_Ryan <57554030+Hunter-link-dev@users.noreply.github.com>
2020-02-29 11:58:41 +0800

    Create main.js

---
+++ mxg-msm-vuex/src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+Vue.use(ElementUI);
+
+import './permission.js'
+
+Vue.config.productionTip = process.env.NODE_ENV === "production";
+console.log(process.env.VUE_APP_SERVICE_URL);
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
