e40db36952f0ca2f6b99a108873d408a28b12adf
Douglas_Ryan <57554030+Hunter-link-dev@users.noreply.github.com>
2020-03-03 19:20:54 +0800

    Create state.js

---
+++ Network take-away/src/store/state.js
@@ -0,0 +1,15 @@
+export default {
+    latitude: 40.10038, // 纬度
+    longitude: 116.36867, // 经度
+    address:{},
+    categorys:[],
+    shops:[],
+    userInfo:{}, //用户信息
+
+    goods:[],
+    ratings:[],
+    info:{},
+    cartFoods: [], // 购物车中食物的列表
+
+    searchShops: [], // 搜索得到的商家列表
+}
