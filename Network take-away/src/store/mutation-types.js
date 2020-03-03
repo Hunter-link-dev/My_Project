daaaa02a4cc1b4d3da1c0b7574615cf3e9088a79
Douglas_Ryan <57554030+Hunter-link-dev@users.noreply.github.com>
2020-03-03 19:21:00 +0800

    Create mutation-types.js

---
+++ Network take-away/src/store/mutation-types.js
@@ -0,0 +1,16 @@
+export const RECEIVE_ADDRESS = 'receive_address'
+export const RECEIVE_CATEGORYS = 'receive_categorys'
+export const RECEIVE_SHOPS = 'receive_shops'
+export const RECEIVE_USERINFO = 'receive_userinfo'
+export const RESET_USER_INFO = 'reset_user_info'
+
+export const RECEIVE_GOODS = 'receive_goods'
+export const RECEIVE_RATINGS = 'receive_ratings'
+export const RECEIVE_INFO = 'receive_info'
+
+export const RECREMENT_FOOD_COUNT = 'recrement_food_count'
+export const DECREMENT_FOOD_COUNT = 'decrement_food_count'
+
+export const CLEAR_COUNT = 'clear_count'
+
+export const RECEIVE_SEARCH_SHOPS = 'receive_search_shops' // 接收搜索的商家数组
